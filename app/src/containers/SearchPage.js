23ea2aee67b176b05a5e43f6e5bcb02a4fc1aa42
Medyen <medianak@hotmail.co.uk>
2021-03-09 14:23:17 +0000

    Fixed Cors Issue

--- app/src/containers/SearchPage.js
+++ app/src/containers/SearchPage.js
@@ -2,37 +2,12 @@ import React, { Component }  from 'react';
 // import VideoGameList from '../components/VideoGameList';
 var axios = require('axios');
 
-// const SearchPage = (props) => {
-//   const [input, setInput] = useState('');
-//   const [videoGameList, setVideoGameList] = useState();
-//   
-
- 
-
-//   const onClick = async () => {
-//     fetchData()
-//   }
-	
-//   // const onChange = (e) => {
-//   //   setInput(e.target.value)
-//   //   console.log(input)
-//   // }
-
-//   return (
-//     <>
-      
-//       <VideoGameList videoGameList={videoGameList}/>
-//     </>
-//    );
-//   }
-// export default SearchPage
-
 export default class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-  
+
   state = {
     search: '',
     url: 'https://cors-anywhere.herokuapp.com/https://www.giantbomb.com/api/characters/?api_key=c8c34267ec67785f30773943e5d622ac3f49c4c2&filter=name:'
@@ -42,7 +17,12 @@ export default class SearchPage extends Component {
 
   fetchData = async () => {
     console.log("Fetching...")
-    const response = await axios(this.state.url + this.state.search)
+    const url = this.state.url + this.state.search
+    const response = await axios({
+      method: 'get',
+      url: url,
+      headers: {'Content-Type': 'text/plain'}
+    })
     const data = await response.data
     console.log(data)
   }
